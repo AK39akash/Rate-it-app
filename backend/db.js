f115c3a8de66b547035165fd80b23763a5f22235
AK39akash <129418539+AK39akash@users.noreply.github.com>
2026-01-13 20:55:23 +0530

    Switch database connection to PostgreSQL

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
 });
 
 module.exports = sequelize;
