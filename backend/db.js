da15c22cf7ff536ed5c9af3bcdd0531f3a8eac3a
AK39akash <129418539+AK39akash@users.noreply.github.com>
2026-03-02 16:28:35 +0530

    Fixed production DB connection

--- backend/db.js
+++ backend/db.js
@@ -19,7 +19,11 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     min: 0,
     acquire: 30000,
     idle: 10000,
-  }
+    evict: 10000,
+  },
+  retry: {
+    max: 5,
+  },
 })
 
 module.exports = sequelize;
